b4562d0a51d6eb77c5ef970e68d97c21d8ce2f48
OksiG <okcana693@mail.ru>
2019-06-11 11:54:09 +0300

    slider task

---
+++ element-search/slider/task.js
@@ -0,0 +1,33 @@
+const prev = document.querySelector('.slider__arrow_prev'); 
+const next = document.querySelector('.slider__arrow_next'); 
+const sliders = document.querySelectorAll('.slider__item');
+
+prev.onclick = function() {
+    for (let i = sliders.length; i < sliders.length; i--) {
+        if (sliders[i].classList.contains( 'slider__item_active' )) {
+            sliders[i].classList.remove( 'slider__item_active' );
+            if (i === 0) {
+                i = sliders.length - 1;
+            } else {
+                i -= 1;
+            }
+            sliders[i].classList.add( 'slider__item_active' );
+            return;
+        }
+    }
+}
+
+next.onclick = function() {
+    for (let i = 0; i < sliders.length; i++) {
+        if (sliders[i].classList.contains( 'slider__item_active' )) {
+            sliders[i].classList.remove( 'slider__item_active' );
+            if (i === sliders.length - 1) {
+                i = 0;
+            } else {
+                i += 1;
+            }
+            sliders[i].classList.add( 'slider__item_active' );
+            return;
+        }
+    }
+}

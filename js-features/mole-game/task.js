f702a538ccabe2dc12f865a975fc59ee27f4acaf
OksiG <okcana693@mail.ru>
2019-06-12 17:38:20 +0300

    correct mole game

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,33 +1,29 @@
 const dead = document.getElementById("dead");
 const lost = document.getElementById("lost");
 
-const addDead = function() {
-    dead.textContent ++;
-}
-
-const addLost = function() {
-    lost.textContent ++;
-}
-
 function getHole( index ) {
     return document.getElementById(`hole${index}`);
 }
 
-for (index = 0; index < 9; index++) {
+for (let index = 1; index < 9; index++) {
     let hole = getHole( index );
+    hole.onclick = function() {
+        if (hole.className.includes( 'hole_has-mole' )) {
+            dead.textContent ++; 
+            if (dead.textContent >= 10) {
+                alert('Победа!');
+                dead.textContent === 0;
+                lost.textContent === 0;
+                return;
+            } 
+        } else {
+            lost.textContent ++;
+            if (lost.textContent >= 5) {
+                alert('Вы проиграли!');
+                dead.textContent === 0;
+                lost.textContent === 0;
+                return;
+            } 
+        }
+    }  
 }
-
-if (hole.className.includes( 'hole_has-mole' )) {
-    if (dead >= 10) {
-        alert('Победа!');
-    } else {
-        hole.onclick = addDead;
-    }
-} else {
-    if (lost >= 5) {
-        alert('Вы проиграли!');
-    } else {
-        hole.onclick = addLost;
-    }
-}
-

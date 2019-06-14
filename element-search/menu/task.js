0943a4987e0bc2f2556f7ffaa65139521519cdc3
OksiG <okcana693@mail.ru>
2019-06-14 23:51:38 +0300

    task menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,2 +1,13 @@
 const menu = document.querySelectorAll("ul.menu_main li");
 const menuSub = document.querySelectorAll("ul.menu_sub li");
+
+for (let i = 0; i < menuSub.length; i++) {
+    menu.addEventListener("click", function() {
+        if(menuSub[i].classList.contains('menu_active')) {
+            menuSub[i].classList.remove('menu_active');
+        }
+        else {
+            menuSub[i].classList.add('menu_active');
+        }  
+    });
+}

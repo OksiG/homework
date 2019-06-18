2c18132fa49c7dacec0eaf8cfc9b83e016a25634
OksiG <okcana693@mail.ru>
2019-06-18 23:03:20 +0300

    correct tabs

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,20 +1,20 @@
 const menu = document.getElementsByClassName("tab");
-const content = document.getElementsByClassName("tab_content");
+const content = document.getElementsByClassName("tab__content");
+
+
 
 for (let i = 0; i < menu.length; i++) {
     menu[i].addEventListener("click", function() {
-        
-        if(menu[i].classList.contains('tab_active')) {
-            menu[i].classList.remove('tab_active');
-            content[i].classList.remove('tab__content_active');
+        for (let j = 0; j < menu.length; j++) {
+            if (menu[j].classList.contains('tab_active')) {
+                menu[j].classList.remove('tab_active');
+                content[j].classList.remove('tab__content_active');
+            } else {
+                menu[i].classList.add('tab_active');
+                content[i].classList.add('tab__content_active');
+            }
         }
-        else {
-            menu[i].classList.add('tab_active');
-            content[i].classList.add('tab__content_active');
-        }  
     });
 }
-
-
 
 

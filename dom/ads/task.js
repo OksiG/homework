1aa0db66019f82eccb9f65e789d0006803663d11
OksiG <okcana693@mail.ru>
2019-06-19 23:40:50 +0300

    add reveal and correct ads

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -3,7 +3,11 @@ let i = 0;
 
 function changeTexts () {
     texts[i].classList.remove('rotator__case_active');
-    i == (texts.length - 1) ? 0 : i++;
+    if (i === (texts.length - 1)) {
+        i = 0;
+    } else {
+        i++;
+    }
     texts[i].classList.add('rotator__case_active');
 }
 
